64d7f22765e02daa3c23daec8adc9b1ebf59e4bb
AABorisov <borisov.andrew.a@gmail.com>
2019-11-18 23:39:09 +0300

    Navbar, Home Page, About Page

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus adipisci alias atque cum dolores earum est, et exercitationem hic illum impedit ipsam ipsum iure laborum, laudantium magni minima modi necessitatibus nemo nulla odit optio pariatur perspiciatis possimus quam qui ratione repellendus sit soluta totam ut veniam veritatis voluptatem. Eveniet, perferendis.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias asperiores aspernatur assumenda aut consequatur corporis deleniti dolor doloribus earum excepturi expedita fugiat illum inventore ipsam iure laboriosam magnam minus, obcaecati praesentium quae quaerat quia quibusdam repellendus repudiandae sed soluta!</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis distinctio expedita magni odio rem sunt.</p>
+  </section>
+</template>
